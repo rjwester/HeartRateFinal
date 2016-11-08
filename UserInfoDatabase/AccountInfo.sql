83d620a5121a23bb1e291607a1f0ffd0269c1c93
TONY-PC\Tony <smith.anthony@uwlax.edu>
2016-11-08 11:04:22 -0600

    DatabaseWork
    Created the databases and stored some info in them. Currently trying to
    implement MVC stuff to be able to add delete and edit stuff in the
    database

---
+++ UserInfoDatabase/AccountInfo.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[AccountInfo]
+(
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50), 
+    CONSTRAINT [PK_AccountInfo] PRIMARY KEY ([Username])
+)
---
+++ UserInfoDatabase/Script.PostDeployment1.sql
@@ -0,0 +1,17 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+Insert into AccountInfo (Username, Password) values ('TestAccount1', 'Test1');
+Insert into UserInfo values (1, 'TestAccount1', 535, 120, '2016-09-01');
+
+
